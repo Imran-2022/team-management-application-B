40ff1c5721c5f00215eef13faf51fb0c7722a319
Imran-2022 <mdimranulhaque.202@gmail.com>
2023-04-03 01:48:17 +0600

    updated status

--- controllers/tasksControllers.js
+++ controllers/tasksControllers.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcrypt')
 const _ = require('lodash');
 const { Tasks } = require('../models/task');
 
-
 module.exports.getTasks = async (req, res) => {
     const tasks = await Tasks.find({})
     res.send(tasks)
@@ -18,5 +17,15 @@ module.exports.addTask = async (req, res) => {
     } catch (err) {
         console.log(err.message, "err0r");
         return res.send({ error: err.message })
+    }
+}
+
+module.exports.updateTaskStatus = async (req, res) => {
+    const id=req.params.id;
+    const updateStatus=req.body.status;
+    const result = await Tasks.updateOne({_id:id}, { status:updateStatus})
+    
+    if(result.modifiedCount>0){
+       return res.send(req.body)
     }
 }
--- routers/tasksRouter.js
+++ routers/tasksRouter.js
@@ -1,8 +1,11 @@
-const { addTask,getTasks } = require('../controllers/tasksControllers');
+const { addTask, getTasks,updateTaskStatus } = require('../controllers/tasksControllers');
 const router = require('express').Router();
 
 router.route('/')
     .get(getTasks)
     .post(addTask)
+
+router.route('/:id')
+    .patch(updateTaskStatus)
 
 module.exports = router;

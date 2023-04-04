150ba6d00b7fd7018a0e5d6dedc8fae6b73054d6
Imran-2022 <mdimranulhaque.202@gmail.com>
2023-04-05 02:14:14 +0600

    connect server mongodb

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', true) // getting error 🤦‍♀️
 
-mongoose.connect(process.env.MONGODB_URL_LOCAL, {})
-    .then(() => console.log("connected to mongodb"))
-    .catch(err => console.error('Mongodb Connection Failed'))
-
-
-// const DB=process.env.MONGODB_SERVER;
-// mongoose.connect(DB)
+// mongoose.connect(process.env.MONGODB_URL_LOCAL, {})
 //     .then(() => console.log("connected to mongodb"))
 //     .catch(err => console.error('Mongodb Connection Failed'))
+
+
+const DB=process.env.MONGODB_SERVER;
+mongoose.connect(DB)
+    .then(() => console.log("connected to mongodb"))
+    .catch(err => console.error('Mongodb Connection Failed'))
 
 const port = process.env.PORT || 3001;
 
